85e7093b1172f02ea104beae5886da80258caa28
Bfirdaniel <bfirdaniel@gmail.com>
2022-10-21 16:43:04 +0700

    Laravel Video 3

--- prakweb_b_203040063_laravel/cobalaravel/resources/views/welcome.blade.php
+++ prakweb_b_203040063_laravel/cobalaravel/resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -130,3 +131,4 @@
         </div>
     </body>
 </html>
+
--- prakweb_b_203040063_laravel/cobalaravel/routes/web.php
+++ prakweb_b_203040063_laravel/cobalaravel/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/about', function () {
+    return 'About';
+});
+
+Route::get('/blog', function () {
+    return 'Post';
 });
