0713e3b32c2d5bc8f8170a0d109871a30e516fd4
Bfirdaniel <bfirdaniel@gmail.com>
2022-10-21 16:47:29 +0700

    Penyelesaian Akhir MVC

--- pw/index.php
+++ pw/index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Just Read It </title>
+    <title>Just Read It !</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <style type="text/css">
       * {
