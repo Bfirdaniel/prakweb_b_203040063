f589537d3e44cd0198e782cb5c5ae54fc44d7e81
Bfirdaniel <76428252+Bfirdaniel@users.noreply.github.com>
2022-12-01 17:47:48 +0700

    Create post.blade.php

--- prakweb_b_203040063_laravel/coba2laravel/resources/views/post.blade.php
+++ prakweb_b_203040063_laravel/coba2laravel/resources/views/post.blade.php
@@ -3,8 +3,21 @@
 @section('container')
 <article>
   <h2>{{ $post->title }}</h2>
+
+  <p>By. <a href="posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+    {{ $post->category->name }}</a></p>
+
+    @if($post->image)
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid ">
+            </div>
+        @else
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid ">
+        @endif
+
   <!-- <h5>{{ $post["author"] }}</h5> -->
   {!! $post->body !!}
+
 </article>
-<a href="/blog">back to posts</a>
+<a href="/blog" class="d-block mt-3">back to posts</a>
 @endsection
