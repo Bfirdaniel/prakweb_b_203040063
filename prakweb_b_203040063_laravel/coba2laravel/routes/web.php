78a030e354ef4abae4502d922fa04a371cefc3b3
Bfirdaniel <76428252+Bfirdaniel@users.noreply.github.com>
2022-12-01 17:43:29 +0700

    Create web.php

--- prakweb_b_203040063_laravel/coba2laravel/routes/web.php
+++ prakweb_b_203040063_laravel/coba2laravel/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 
 /*
@@ -18,18 +22,49 @@ use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
+
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "name" => "Billy Firdaniel",
-        "email" => "Bfirdaniel@gmail.com",
-        "image" => "b1.jpeg"
+        'active' => 'about',
+        "name" => "Billy Firdaniel Nafitra",
+        "email" => "Billy@gmail.com",
+        "image" => "img.jpg"
     ]);
 });
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])
+    ->middleware('auth');
+
+Route::resource(
+    '/dashboard/posts',
+    DashboardPostController::class
+)->middleware('auth');
